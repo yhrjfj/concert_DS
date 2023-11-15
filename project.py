a439240f4288c3e6c80739bf1bd1689c4d4ae3a5
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-15 19:05:54 +0600

    Login through phone number

--- project.py
+++ project.py
@@ -7,8 +7,8 @@ class UserLinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
+    def append(self, data, password):
+        new_node = Node((data, password))
         if not self.head:
             self.head = new_node
             return
@@ -20,28 +20,31 @@ class UserLinkedList:
     def display(self):
         current = self.head
         while current:
-            print(current.data)
+            print(current.data[0])
             current = current.next
 
 class ConcertRegistrationSystem:
     def __init__(self):
         self.user_list = UserLinkedList()
         self.admin_otp_mapping = {}
+        self.user_data_dict = {}
 
-    def register_user(self, name, email, phone):
+    def register_user(self, name, email, phone, password):
         user_data = f"Name: {name}, Email: {email}, Phone: {phone}"
-        self.user_list.append(user_data)
-        return user_data
+        self.user_list.append(user_data, password)
+        self.user_data_dict[email] = (phone, name, password)
+        return email, phone
 
     def generate_otp(self, user_data):
         otp = hash(user_data) % (10 ** 6)  # Simplified OTP generation
         self.admin_otp_mapping[user_data] = otp
         return otp
 
-    def confirm_registration(self, user_data, entered_otp):
-        if user_data in self.admin_otp_mapping:
-            stored_otp = self.admin_otp_mapping[user_data]
-            return stored_otp == int(entered_otp)
+    def confirm_registration(self, email, password, entered_otp):
+        if email in self.user_data_dict:
+            stored_otp = self.admin_otp_mapping[f"Name: {self.user_data_dict[email][1]}, Email: {email}, Phone: {self.user_data_dict[email][0]}"]
+            if stored_otp == int(entered_otp) and self.user_data_dict[email][2] == password:
+                return True
         return False
 
     def display_user_list(self):
@@ -52,32 +55,53 @@ class ConcertRegistrationSystem:
         while True:
             choice = input("Enter 'L' to login or 'R' to register: ")
             if choice.upper() == 'L':
-                email = input("Enter your email: ")
-                phone = input("Enter your phone number: ")
-                user_data = f"Email: {email}, Phone: {phone}"
-                if user_data in self.admin_otp_mapping:
-                    otp = self.admin_otp_mapping[user_data]
-                    entered_otp = input("Enter OTP to login: ")
-                    if otp == int(entered_otp):
+                login_choice = input("Enter 'E' to login with email and password or 'P' to login with phone number: ")
+                if login_choice.upper() == 'E':
+                    email = input("Enter your email: ")
+                    password = input("Enter your password: ")
+                    if self.confirm_registration(email, password, input("Enter OTP: ")):
                         print("Login successful!")
                         return
                     else:
-                        print("Invalid OTP. Please try again.")
+                        print("Invalid email or password. Please try again.")
+                elif login_choice.upper() == 'P':
+                    email = input("Enter your email: ")
+                    if email in self.user_data_dict:
+                        phone = input("Enter your phone number: ")
+                        if phone == self.user_data_dict[email][0]:
+                            print("Login successful!")
+                            return
+                        else:
+                            print("Invalid phone number. Please try again.")
+                    else:
+                        print("User not found. Please register first.")
                 else:
-                    print("User not found. Please register first.")
+                    print("Invalid choice. Please try again.")
             elif choice.upper() == 'R':
                 name = input("Enter your name: ")
                 email = input("Enter your email: ")
                 phone = input("Enter your phone number: ")
-                user_data = self.register_user(name, email, phone)
-                otp = self.generate_otp(user_data)
+                password = input("Enter your password: ")
+                email, phone = self.register_user(name, email, phone, password)
+                otp = self.generate_otp(f"Name: {name}, Email: {email}, Phone: {phone}")
                 print(f"OTP sent to user: {otp}")
                 entered_otp = input("Enter OTP to confirm registration: ")
-                if self.confirm_registration(user_data, entered_otp):
+                if self.confirm_registration(email, password, entered_otp):
                     print("Registration confirmed!")
+                    # Allow user to log in after registering
+                    print("Logging in...")
+                    if email in self.user_data_dict:
+                        phone = input("Enter your phone number: ")
+                        if phone == self.user_data_dict[email][0]:
+                            print("Login successful!")
+                            return
+                        else:
+                            print("Invalid phone number. Please try again.")
+                    else:
+                        print("User not found. Please register first.")
                     return
                 else:
-                    print("Invalid OTP. Registration not confirmed.")
+                    print("Invalid OTP or password. Registration not confirmed.")
             else:
                 print("Invalid choice. Please try again.")
 
